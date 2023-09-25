300682d5c2ef0e833a4bb17203af585760221f27
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2023-09-25 15:35:38 +0700

    env

--- src/app/layout.js
+++ src/app/layout.js
@@ -71,8 +71,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650610783"
+                fullName="Paweennut Prohsoontorn"
                 year="2023"
               />
             </Container>
